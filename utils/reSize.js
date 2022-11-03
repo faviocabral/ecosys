5e7d4a50202d37f99d3e1f38acfcaf58a494a321
faviocabral <faviocabral@gmail.com>
2022-11-03 17:57:48 -0300

    optimize code add per toggle menu

--- utils/reSize.js
+++ utils/reSize.js
@@ -3,18 +3,12 @@ export const AbrirMenu = ()=> {
     let classBody = String(document.querySelector("body").classList) 
 
     if(window.innerWidth > 991){
-      if(!classBody.includes('sidebar-collapse')){ 
-        classBody.split(" ").forEach(item=> (item.length > 0 )? document.querySelector("body").classList.remove(item) : '' )
-        document.querySelector("body").classList.add("sidebar-collapse")
-      }else{ 
-        document.querySelector("body").classList.remove("sidebar-collapse")
-      }
+      classBody.split(" ").forEach(item=> (item.length > 0 )? document.querySelector("body").classList.remove(item) : '' )
+      document.querySelector("body").classList.toggle("sidebar-collapse")
+
     }else{
-      if(!classBody.includes('sidebar-open') ){ 
-        classBody.split(" ").forEach(item=> (item.length > 0 )? document.querySelector("body").classList.remove(item) : '' )
-        document.querySelector("body").classList.add("sidebar-open")
-      }else{ 
-        document.querySelector("body").classList.remove("sidebar-open")
-      }
+      classBody.split(" ").forEach(item=> (item.length > 0 )? document.querySelector("body").classList.remove(item) : '' )
+      document.querySelector("body").classList.toggle("sidebar-open")
+
     }
   }
