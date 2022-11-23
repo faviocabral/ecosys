9a9a08f67c12bd1972941210a8cefeebd41f6827
faviocabral <faviocabral@gmail.com>
2022-11-22 22:45:16 -0300

    change database source

--- knexfile.js
+++ knexfile.js
@@ -8,12 +8,24 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host : 'mysql-94780-0.cloudclusters.net',
-      port : 17425,
-      user : 'admin',	
-      password : 'We3zMFT2',
-      database : 'ecosys'
+      host : 'sql.freedb.tech',
+      port : 3306,
+      user : 'freedb_ecosys',	
+      password : 'cYbHRzdDZ&!9k@C',
+      database : 'freedb_ecosys'
     },
+/*    development: {
+      client: 'mysql',
+      connection: {
+        host : 'mysql-94780-0.cloudclusters.net',
+        port : 17425,
+        user : 'admin',	
+        password : 'We3zMFT2',
+        database : 'ecosys'
+      },
+*/  
+
+
     migrations: {
       directory: './db/migrations'
     },
